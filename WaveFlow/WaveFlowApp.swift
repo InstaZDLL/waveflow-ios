381a83c15b054eccb2c90ad74ca794d0fe75d51f
InstaZDLL <github.105mh@8shield.net>
2026-08-09 16:11:39 +0200

    Initial Commit

---
+++ WaveFlow/WaveFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveFlowApp.swift
+//  WaveFlow
+//
+//  Created by Nayeon Im on 09.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct WaveFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaveFlowTests/WaveFlowTests.swift
@@ -0,0 +1,19 @@
+//
+//  WaveFlowTests.swift
+//  WaveFlowTests
+//
+//  Created by Nayeon Im on 09.08.2026.
+//
+
+import Testing
+@testable import WaveFlow
+
+struct WaveFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
